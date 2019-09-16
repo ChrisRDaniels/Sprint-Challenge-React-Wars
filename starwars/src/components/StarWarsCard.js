824c92642d109b6f6a757127a85af27bccb2e780
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-16 17:02:50 -0600

    styled card with styled-components and changed minwidth for best ui experience

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -6,15 +6,15 @@ const StarWarsCard = (props) => {
       <>
          <Container>
                 <Row style={{ justifyContent: "center" }} >
-                    <Col xs="4" sm="6" lg="10" style={{height: "45vh", marginBottom: "10vh"}} >
-                        <Card >
-                            <CardBody style={{backgroundColor: "#C4C6D2", textAlign: "center", boxShadow: "10px 10px 10px 0px black" }}>
+                    <Col xs="4" sm="6" lg="10" style={{height: "45vh", minWidth: "240px", marginBottom: "10vh"}} >
+                        <Card style={{backgroundColor: "darkred", borderRadius: "40%"}}>
+                            <CardBody style={{backgroundColor: "#C4C6D2", borderRadius: "45%", minWidth: "200px", textAlign: "center"}}>
                                 <CardImg style={{height: "50%", width: "50%", margin: "0 auto", border: "none"}}className="StarWarsLogo" alt="Star Wars Logo" src={"https://i.redd.it/tuhtobyiqoc01.png"} />
                                 <CardTitle><h1>{props.name}</h1></CardTitle>
                                 <CardSubtitle><p>Gender: {props.gender}</p></CardSubtitle>
                                 <CardSubtitle><p>Hair Color: {props.hair_color}</p></CardSubtitle>
                                 <CardSubtitle><p>Height: {props.height} cm</p></CardSubtitle>
-                                <CardSubtitle><p>Mass: {props.mass}</p></CardSubtitle>
+                                <CardSubtitle><p>Weight: {props.mass} kg</p></CardSubtitle>
                             </CardBody>
                         </Card>
                     </Col>
